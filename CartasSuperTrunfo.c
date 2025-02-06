9c2de44239e3ef6bd44028efd8c0ad7e035cbe08
Fannihernandes <202501107711@alunos.estacio.br>
2025-02-06 22:14:32 +0000

    Melhorei comando

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,47 +1,40 @@
 #include <stdio.h>
 
 int main() {
-    int codigo;
-    char nome[20];
+    char codigo_cidade[4];
+    char nome_cidade[20];
     int populacao;
     float area;
     float PIB; 
-    int pontos;
+    int numero_pontos_turisticos;
 
     printf("Digite o código da cidade: \n");
-    scanf("%d", &codigo);
-    
-    while (getchar() != '\n');
+    scanf("%s", &codigo_cidade);
 
     printf("Digite o nome da cidade: \n");
-    scanf("%s", &nome);
-
-    while (getchar() != '\n');
+    scanf("%s", &nome_cidade);
 
     printf("Digite a população dessa cidade: \n");
     scanf ("%d", &populacao);
 
-    while (getchar() != '\n');
-
     printf("Digite a área dessa cidade em KM²: \n");
-    scanf("%f", &area);
-
-    while (getchar() != '\n');
+    scanf(" %f", &area);
 
     printf("Digite em R$ o PIB dessa cidade: \n");
-    scanf("%f", &PIB);
-
-    while (getchar() != '\n');
+    scanf(" %f", &PIB);
 
     printf("Digite o número de pontos turísticos dessa cidade: \n");
-    scanf("%d", &pontos);
+    scanf(" %d", &numero_pontos_turisticos);
 
-    printf("Código da cidade: %d\n", codigo);
-    printf("Nome da cidade: %s\n", nome);
+    printf("Código da cidade: %s\n", codigo_cidade);
+    printf("Nome da cidade: %s\n", nome_cidade);
     printf("População: %d\n", populacao);
-    printf("Área (KM²): %0.2f\n", area);
-    printf("PIB (R$): %0.2f\n", PIB);
-    printf("Número de pontos turísticos: %d\n", pontos);
+    printf("Área (KM²): %0.3f\n", area);
+    printf("PIB (R$): %0.3f\n", PIB);
+    printf("Número de pontos turísticos: %d\n", numero_pontos_turisticos);
+
+
+
 
 
     return 0;
